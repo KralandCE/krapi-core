48b97ee23d5e32b39875f265d948c2b7256549bf
Ricorei <contact.adpmbit7@omnimail.fr>
2016-12-22 19:08:52 +0100

    Fix #6.

--- ki-event-parser/src/main/java/org/kralandce/krapi/core/parser/Constantes.java
+++ ki-event-parser/src/main/java/org/kralandce/krapi/core/parser/Constantes.java
@@ -1,6 +1,11 @@
 package org.kralandce.krapi.core.parser;
 
-public interface Constantes {
+public final class Constantes {
+
+	private Constantes()
+	{
+
+	}
 
 	public static final String UTF_8 = "UTF-8";
 	public static final String ISO = "ISO-8859-1";
