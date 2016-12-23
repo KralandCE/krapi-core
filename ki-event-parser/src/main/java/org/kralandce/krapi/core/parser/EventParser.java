1ae24cdd6247ae92138ee6498b76bb76ea14b204
Hello-Gitty <mister.bloodmoon@gmail.com>
2016-12-24 00:07:27 +0100

    Intégration de la lecture des pages en détail avec le passage de la sécurité: -lecture raw des datas -exploration de la page pour trouver le nombre de page -tests cas réel

--- ki-event-parser/src/main/java/org/kralandce/krapi/core/parser/EventParser.java
+++ ki-event-parser/src/main/java/org/kralandce/krapi/core/parser/EventParser.java
@@ -26,8 +26,6 @@ public class EventParser {
 
     private Events theEvenements;
 
-    //private static final String LOCAL_PAGE = "file:///C:/Users/Dart/Desktop/Kraland/workon/Kraland%20Interactif%20-%20Cybermonde%20-%20%C3%89v%C3%A9nements.htm";
-    private static final String LOCAL_PAGE = "";
     /*
      * Algo:
      * 
@@ -49,130 +47,10 @@ public class EventParser {
         this.theEvenements = new Events();
         this.theEvenements.setJour(localDate);
         
-        Connection conn = Jsoup.connect("http://www.kraland.org/main.php?p=4_4");
         
-        Document    
-        doc =  conn.url("http://www.kraland.org/main.php?p=4_4&t=211cc3&a=2&p1=10").referrer("http://www.kraland.org/main.php?p=4_4&t=211cc3&a=2&p1=10").get();
-        
-        System.out.println(doc);
-        
-        // the date of the event that we need
-        String eventDate = localDate.toString(); //"2016-03-22";// 
-        
-        String beforeEventDate =  localDate.minusDays(1).toString(); //"2016-03-21"; //
-        
-
-        // GET a PAGE
-        
-        // Lookup for DATE
-        Elements listJour = doc.getElementsByClass("forum-c3");
-        int page = 0;
-        // Lookup for page list
-        if (listJour.size() > 1) {
-            Element pageList = listJour.get(0);
-            pageList = pageList.previousElementSibling();
-            // nombre de page
-            page = pageList.child(0).childNodeSize()/2;
-        }
-        
-        boolean stop = false;
-        do {
-          
-          stop = parseCenter(listJour, eventDate, beforeEventDate);
-          // TODO need plus eleguant
-          if (!stop) {
-              page = page - 1;
-              //    Jsoup.connect("http://www.kraland.org/main.php?p=4_4_"+page";
-              doc = getPageByPass(LOCAL_PAGE);
-              listJour = doc.getElementsByClass("forum-c3");
-          }
-        // stop if no page or event found for the day before
-        } while (page != 0 && !stop);
-        
-        return this.theEvenements;
-    }
-    
-    public boolean parseCenter(Elements listJour, String day, String theDayBefore) {
-        boolean theDayBeforeFound = false;
-         
-        
-        Element found = null;
-        for (Element el : listJour) {
-            String valDateNode = Util.getFullText(el);
-            if (day.equalsIgnoreCase(valDateNode)) {
-                found = el;
-            }
-        }
-
-        if (found != null) {
-            Element sib = found.nextElementSibling();
-            boolean sep = false;
-            while (sib != null && !sep) {
-                if (!sib.hasClass("forum-c3") && sib.childNodeSize() == 3) {
-                    Event ev = parseNode(day, sib);
-                    this.theEvenements.add(ev);
-                } else {
-                    // on a fini de traiter les évènements du jour
-                    sep = true;
-                    // TODO to be Test
-                    theDayBeforeFound = theDayBefore.equalsIgnoreCase(Util.getFullText(sib));
-                }
-                sib = sib.nextElementSibling();
-            }
-        }
-        return theDayBeforeFound;
-    }
-    
-    
-    private static Connection copyCookie(Connection.Response res, Connection conn) {
-        if (res != null && conn != null) {
-            Map<String, String> cookies = res.cookies();
-            for (Map.Entry<String, String> entry : cookies.entrySet()) {
-                conn.cookie(entry.getKey(), entry.getValue());
-               // System.out.println(entry.getKey());
-            }
-        }
-        return conn;
-    }
-    
-    private static String getHiddenT(Document doc) {
-        
-        return doc.getElementById("report-col3").getElementsByAttributeValue("name", "t").get(0).attr("value");
-    }
-    
-    /**
-     * 
-     * @return
-     * @throws IOException
-     */
-    private static Connection.Response authentificationKi() throws IOException {
-        Connection.Response res = null;
-        if (EventParserParam.AUTHENFICATION.isVal()) {
-            res = Jsoup.connect("http://www.kraland.org/main.php?p=5&a=100")
-                    .data("p1", EventParserParam.KI_SLAVE_LOGIN.getVal(), "p2", EventParserParam.KI_SLAVE_PASS.getVal(),
-                            "Submit", "Ok")
-                    .method(Method.POST).execute();
-        } else {
-            res = Jsoup.connect("http://www.kraland.org/main.php").method(Method.GET).execute();
-        }
-        return res;
-    }
-    
-    
-    
-    /**
-     * Only for test for now.
-     * 
-     * @param args
-     * @throws MalformedURLException
-     * @throws IOException
-     */
-    public static void main(String[] args) throws MalformedURLException, IOException {
-        //Util.toPrettyJson(new EventParser().proceed());
         Connection conn = null;
         Connection.Response res = null;
         Document doc = null;
-        
         
         
         Connection.Response resref = authentificationKi();
@@ -213,16 +91,147 @@ public class EventParser {
                 
         res = conn.execute();
         doc = res.parse();
-        System.out.println(doc);
+        //System.out.println(doc); 
+        
+        
+        //doc = getPageByPass(LOCAL_PAGE);
         
         // là on a la page principale de travail.
         // On doit ensuite faire des get des autres pages avec le bon referrer
         // les cookies et le bon lien 
         // et c'est gagné.
+
         
+        //System.out.println(doc);
         
+        // the date of the event that we need
+        String eventDate = localDate.toString(); //"2016-03-22";//  
+        
+        String beforeEventDate = localDate.minusDays(1).toString(); // "2016-03-21"; //
         
 
+        // GET a PAGE
+        
+        // Lookup for DATE
+        Elements listJour = doc.getElementsByClass("forum-c3");
+        
+        Elements selectPage = doc.getElementsByClass("forum-selpage");
+        
+        int page = 0;
+        // Lookup for page list
+        if (!selectPage.isEmpty()) {
+            Element pageList = selectPage.get(0);
+            pageList = pageList.parent();
+            // nombre de page
+            page = pageList.childNodeSize()/2;
+        }
+        
+
+
+        boolean stop = false;
+        do {
+          
+          stop = parseCenter(listJour, eventDate, beforeEventDate);
+          // TODO need plus eleguant
+          if (!stop) {
+              page = page - 1;
+              conn = Jsoup.connect("http://www.kraland.org/main.php?p=4_4_"+page);
+              conn.referrer(lien)
+              .header("Host", "www.kraland.org").method(Method.GET);
+              conn = copyCookie(resref, conn);
+              res = conn.execute();
+              doc = res.parse();
+              listJour = doc.getElementsByClass("forum-c3");
+          }
+        // stop if no page or event found for the day before
+        } while (page != 0 && !stop);
+
+        return this.theEvenements;
+    }
+    
+
+    
+    public boolean parseCenter(Elements listJour, String day, String theDayBefore) {
+        boolean theDayBeforeFound = false;
+         
+        
+        Element found = null;
+        for (Element el : listJour) {
+            String valDateNode = Util.getFullText(el);
+            if (day.equalsIgnoreCase(valDateNode)) {
+                found = el;
+            }
+        }
+
+        if (found != null) {
+            Element sib = found.nextElementSibling();
+            boolean sep = false;
+            while (sib != null && !sep) {
+                if (!sib.hasClass("forum-c3") && sib.childNodeSize() == 3) {
+                    Event ev = parseNode(day, sib);
+                    this.theEvenements.add(ev);
+                } else {
+                    // on a fini de traiter les évènements du jour
+                    sep = true;
+                    // TODO to be Test
+                    theDayBeforeFound = theDayBefore.equalsIgnoreCase(Util.getFullText(sib));
+                }
+                sib = sib.nextElementSibling();
+            }
+        }
+        return theDayBeforeFound;
+    }
+    
+
+    /**
+     * Put cookies from a response into a connection
+     * @param res
+     * @param conn
+     * @return
+     */
+    private static Connection copyCookie(Connection.Response res, Connection conn) {
+        if (res != null && conn != null) {
+            Map<String, String> cookies = res.cookies();
+            for (Map.Entry<String, String> entry : cookies.entrySet()) {
+                conn.cookie(entry.getKey(), entry.getValue());
+            }
+        }
+        return conn;
+    }
+    
+    private static String getHiddenT(Document doc) {
+        return doc.getElementById("report-col3").getElementsByAttributeValue("name", "t").get(0).attr("value");
+    }
+    
+    /**
+     * Handle authentification if needed for cookie
+     * @return
+     * @throws IOException
+     */
+    private static Connection.Response authentificationKi() throws IOException {
+        Connection.Response res = null;
+        if (EventParserParam.AUTHENFICATION.isVal()) {
+            res = Jsoup.connect("http://www.kraland.org/main.php?p=5&a=100")
+                    .data("p1", EventParserParam.KI_SLAVE_LOGIN.getVal(), "p2", EventParserParam.KI_SLAVE_PASS.getVal(),
+                            "Submit", "Ok")
+                    .method(Method.POST).execute();
+        } else {
+            res = Jsoup.connect("http://www.kraland.org/main.php").method(Method.GET).execute();
+        }
+        return res;
+    }
+    
+    
+    
+    /**
+     * Only for test for now.
+     * 
+     * @param args
+     * @throws MalformedURLException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        System.out.println(Util.toPrettyJson(new EventParser().proceed()));
     }
 
     private Document getPageByPass(String url) throws MalformedURLException, IOException {
@@ -263,7 +272,7 @@ public class EventParser {
             ville = Util.getText(loca.childNode(2));
         }
         // TODO Trouver comment récupérer proprement les images etc.
-        result.setData(Util.getFullText(node.childNode(2)));
+        result.setData(node.childNode(2).toString());
 
         String heure = Util.getText(node.childNode(1));
         heure = heure.replace("\u00A0", "").trim(); // Espace insecable pourri
@@ -272,7 +281,6 @@ public class EventParser {
         result.setVille(ville);
         result.setProvince(province);
         result.setEmpire(empire);
-        System.out.println(Util.toPrettyJson(result));
         return result;
     }
 
