b850c9f7fb61b9875aed6bd7e73432021b76d30c
Ricorei <contact.adpmbit7@omnimail.fr>
2017-01-06 18:56:59 +0100

    KHttpClient must open a session to crawl Kraland ( else it is rejected by security measures ). Session is anonymous if no login or password are provided.

--- ki-common/src/main/java/org/kralandce/krapi/core/KHttpClient.java
+++ ki-common/src/main/java/org/kralandce/krapi/core/KHttpClient.java
@@ -25,46 +25,48 @@ public final class KHttpClient {
     private final Map<String, String> cookies;
     private String referrer;
     private Document currentPage;
-    private boolean requireAuthentication;
+    private String login;
+    private String password;
+    private Connection.Response currentResult;
 
     public KHttpClient() {
         this.cookies = new HashMap<>();
 
         this.referrer = null;
         this.currentPage = null;
-        this.requireAuthentication = false;
+        this.currentResult = null;
+        this.login = null;
+        this.password = null;
     }
 
-    public void setRequireAuthentication(boolean requireAuth) {
-        this.requireAuthentication = requireAuth;
-    }
-
-    public void connect() {
-        if( isConnected() ) {
+    public void openSession() {
+        if( isSignedIn() ) {
             return;
         }
 
-        if( !this.requireAuthentication ) {
-            return;
+        if( this.login != null && this.password != null ) {
+            execute(CommonConst.URL_AUTH_KRALAND, connection -> {
+                connection.method(Connection.Method.POST).data("p1", this.login, "p2", this.password, "Submit", "Ok");
+            });
+        }
+        else {
+            execute(CommonConst.URL_KRALAND_MAIN);
         }
 
-        try {
-            Map<String, String> tempCookies = Util.authentificationKi();
-            if( tempCookies != null ) {
-                this.cookies.putAll(tempCookies);
-            }
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
+        this.cookies.clear();
+        this.cookies.putAll(this.currentResult.cookies());
     }
 
-    public boolean isConnected() {
-        return this.cookies.get("PHPSESSID") != null //
+    public boolean isSignedIn() {
+        return isSessionOpen() //
             && this.cookies.get("login") != null  //
             && this.cookies.get("id") != null //
             && this.cookies.get("password") != null //
             && this.cookies.get("pc_id") != null; //
+    }
+
+    public boolean isSessionOpen() {
+        return this.cookies.get("PHPSESSID") != null;
     }
 
     public void disconnect() {
@@ -73,34 +75,37 @@ public final class KHttpClient {
         this.currentPage = null;
     }
 
-    public KHttpClient navigate(String url) {
+    private void execute(String url) {
         execute(url, connection -> {});
-        return this;
-    }
-
-    public KHttpClient navigate(String url, Consumer<Connection> connectionConsumer) {
-        execute(url, connectionConsumer);
-        return this;
     }
 
     private void execute(String url, Consumer<Connection> connectionConsumer) {
-        checkState(!this.requireAuthentication || isConnected());
         Connection connection = Util.getConnection(url, this.cookies, this.referrer);
         connectionConsumer.accept(connection);
 
-        logger.debug("Visiting {} via {}", url, connection);
-
-        Connection.Response result;
+        logger.debug("Visiting {}", url);
 
         try {
             this.currentPage = null;
-            result = connection.execute();
-            this.currentPage = result.parse();
+            this.currentResult = connection.execute();
+            this.currentPage = this.currentResult.parse();
             this.referrer = url;
         }
         catch(IOException e) {
             logger.error("Could not open {}", url);
         }
+    }
+
+    public KHttpClient navigate(String url) {
+        checkState(isSessionOpen());
+        execute(url);
+        return this;
+    }
+
+    public KHttpClient navigate(String url, Consumer<Connection> connectionConsumer) {
+        checkState(isSessionOpen());
+        execute(url, connectionConsumer);
+        return this;
     }
 
     public Optional<Document> getPage() {
